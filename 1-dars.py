b12282cde67b21fc143a901e7f115f755651cd49
Ahrorbek <93393357+mamadjonovahror@users.noreply.github.com>
2021-11-04 21:25:12 +0500

    1 dars dasturlash asoslalari
    yangi dars 1 dars

---
+++ 1-dars.py
@@ -0,0 +1,7 @@
+#01 dars Mamadjon Ahrorbek sana 04.11.2021 
+#Biz bugun Hello World ! chikarishni organamiz sizlarga ko;rsatib beraman 
+
+print ('Hello World !') # Buyerda kodni togri yozish kerak () qolib ketmaskik kerak 
+
+#Eibor uchun  rahmat !!!!
+
