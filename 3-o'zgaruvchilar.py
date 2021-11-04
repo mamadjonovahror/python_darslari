b870b636148411a8503116ee0fe7f6da014c52fd
Ahrorbek <93393357+mamadjonovahror@users.noreply.github.com>
2021-11-04 23:08:00 +0500

    Create 3-o'zgaruvchilar.py
    3 dars o'zgaruvchilar

---
+++ 3-o'zgaruvchilar.py
@@ -0,0 +1,24 @@
+# 03 Dars O'zgaruvchilar 
+
+# Mamadjonov Ahror sana 04.11.2021
+
+
+ism = 'Ahad'
+
+yosh = 18
+
+familiya = 'Davlatov'
+
+print(ism)
+
+#Misol uchun 
+ 
+radius = 25
+
+Pi = 3.14
+
+aylana_yuzi = Pi * radius**2
+
+print('Radius' , radius, " ga teng aylananing yuzi = " , aylana_yuzi )
+
+# Bugun shuni organamaniz !! darsimiz tugadi !
