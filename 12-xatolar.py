8a5189e3a61c589866cbe769fc02ca6a95710480
Ahrorbek <93393357+mamadjonovahror@users.noreply.github.com>
2021-11-04 23:27:33 +0500

    12_Dars Xatolar bilan ishlash
    12

---
+++ 12-xatolar.py
@@ -0,0 +1,47 @@
+# 12 Dars Eng ko'p uchraydigon xatolar sana 04.11.2021
+# Mamadjonov Ahror 
+
+#Sinteks xatolik
+
+#print ' Hello World !' #buyerda sinteks xatolik boldi yani () qolib kettk
+
+runfile('D:/Python/12-xatolar.py', wdir='D:/Python')
+  File "D:\Python\12-xatolar.py", line 6
+    print ' Hello World !'
+          ^
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(' Hello World !')?
+
+
+
+ EOL ELOF Xatolik 
+
+print (' Hello World ! 
+       
+     Buyerda esa EOL xatoligi esa funktsiya oxirida qavsni yopish esdan chiqqanda yuzaga keladi.
+       
+       runfile('D:/Python/12-xatolar.py', wdir='D:/Python')
+  File "D:\Python\12-xatolar.py", line 18
+    print (' Hello World !
+                           ^
+SyntaxError: EOL while scanning string literal
+
+
+print( " Hello World "
+      
+      SyntaxError: unexpected EOF while parsing
+EOF xatoligining muammoli tarafi, Python aynan qaysi funktsiya yopilmay qolganini ko'rsata olmaydi, ya'ni kodni sinchiklab ko'zdan kechirib chiqish talab qilinadi.
+
+# Joy tashlashda xotolik 
+ 
+   print 
+
+  ^
+
+
+JOY TASHLASHDA XATOLIK
+Python tilida qator boshidan yoki joy tashlab yozish muhim ahamiyatga ega. Qator boshidan asossiz joy qoldirish IndentationError ga olib keladi.
+
+Quyidagi kodga e'tibor bering, qator boshida 1 dona bo'sh joy qolgani uchunoq Spyder muhiti xatolikni aniqlab, qizil bilan belgilab qo'ydi.
+
+
+
