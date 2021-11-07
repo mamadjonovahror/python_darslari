b4ff401a46ca130511d74136e544ea4f6f6e0a48
Ahrorbek <93393357+mamadjonovahror@users.noreply.github.com>
2021-11-07 13:33:06 +0500

    Create 13-lugat.py
    13- dars

---
+++ 13-lugat.py
@@ -0,0 +1,42 @@
+# Dars Lugat bilan tanishuv sana 06.11.2021
+
+# Mamadjonov Ahrorbek 
+
+# Lug'at bilan tanishamiz
+
+car_0 = {'model' :'ferrari' , 'rang' :'qizil'}
+print(car_0['model'])
+print(car_0['rang' ])
+
+en_uz = {'apple' : 'olma', 'apricot' : "o'rik" , 'banana' : 'banan', 'sedan' :'kuzov'}
+print(en_uz['apple'])
+print(en_uz['apricot'])
+print(en_uz['banana'])
+print(en_uz['sedan'])      
+
+mevalar = {'tarvuz' : 1000 , 'banan' : 15000 , 'olma' : 22000 , 'qovun' : 10000 }
+print(f" Banan sotib oldim {mevalar['tarvuz']} so'm ga !. ")
+
+talaba_0 ={'ism_f':'ahror_mamadjonov' , 't_yil':' 2001' , 'yosh':' 21'}
+print(f"{talaba_0['ism_f']. title()},\
+   {talaba_0['t_yil']} - yilda tugilagan ,\
+   {talaba_0['yosh']} yoshda !.")    
+   
+# Bu yerda yangi malumotlar qoshseg bo'ladi !
+
+talaba_0 ['kurs'] = 4
+talaba_0['fakultet'] = 'Organika'
+talaba_0['sohasi'] = 'Neft'
+talaba_0['ism_f'] = 'Dolimov Zakariyo'
+
+# Bu funksiya orqali istalgan so'zni o'chirib tashlasa bo'ladi 'del' . 
+
+
+del en_uz [ 'apple']
+
+telefonlar = {
+    'ahror' : 'Redmi not 9 t',
+    'dolimov' : 'iphone xr',
+    "xo'ja " : 'samsung' ,
+    'emil': 'redmi'
+    }
